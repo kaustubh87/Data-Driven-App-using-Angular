57c9e399524916df144e9de49d29929a4e62d495
kvin1087 <kaustubh87@gmail.com>
2017-08-30 12:18:59 -0500

    Added extra fields to the user table

--- angularregistration/js/services/authentication.js
+++ angularregistration/js/services/authentication.js
@@ -12,6 +12,14 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', function($rootSc
                 user.email,
                 user.password
             ).then(function(regUser) {
+                var regRef = ref.child('users')
+                    .child(regUser.uid).set({
+                        date: firebase.database.ServerValue.TIMESTAMP,
+                        regUser: regUser.uid,
+                        firstname: user.firstname,
+                        lastname: user.lastname,
+                        email: user.email
+                    });
                 $rootScope.message = "Hi " + $rootScope.user.firstname + ' ' + " Thanks for registering";
             }).catch(function(error) {
                 $rootScope.message = error.message;
