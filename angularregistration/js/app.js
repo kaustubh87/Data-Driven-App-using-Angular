2901d5fb25a95aa6cebdc82407216cb29c68323c
kvin1087 <kaustubh87@gmail.com>
2017-08-30 09:01:29 -0500

    Added pretty url

--- angularregistration/js/app.js
+++ angularregistration/js/app.js
@@ -1,20 +1,24 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-    when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginController'
-        })
-        .when('/register', {
-            templateUrl: 'views/register.html',
-            controller: 'RegistrationController'
-        })
-        .when('/success', {
-            templateUrl: 'views/success.html',
-            controller: 'SuccessController'
-        })
-        .otherwise({
-            redirectTo: '/login'
-        });
-}]);
+myApp.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+        when('/login', {
+                templateUrl: '../../angularregistration/views/login.html',
+                controller: 'LoginController'
+            })
+            .when('/register', {
+                templateUrl: '../../angularregistration/views/register.html',
+                controller: 'RegistrationController'
+            })
+            .when('/success', {
+                templateUrl: '../../angularregistration/views/success.html',
+                controller: 'SuccessController'
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+
+
+    }
+]);
