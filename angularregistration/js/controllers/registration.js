2c852cf289673b3676e6629fc10b6a5568bc9737
kvin1087 <kaustubh87@gmail.com>
2017-08-30 12:19:24 -0500

    Authentication factory register method invoked

--- angularregistration/js/controllers/registration.js
+++ angularregistration/js/controllers/registration.js
@@ -1,13 +1,8 @@
 myApp.controller('RegistrationController', ['$scope', 'Authentication', function($scope, Authentication) {
 
-
-
     $scope.register = function() {
-
         /* Registering users into firebase */
 
         Authentication.register($scope.user);
-
-
     };
 }]);
