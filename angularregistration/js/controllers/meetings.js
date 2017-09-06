867f75f714b254417d29f60b21e058133d4f256b
kvin1087 <kaustubh87@gmail.com>
2017-09-06 15:58:47 -0500

    Added new MeetingsController

--- angularregistration/js/controllers/meetings.js
+++ angularregistration/js/controllers/meetings.js
@@ -1,3 +1,14 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-    $scope.message = "Success!!";
+myApp.controller('MeetingsController', ['$scope', '$firebaseAuth', '$firebaseArray', function($scope, $firebaseAuth, $firebaseArray) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    auth.$onAuthStateChanged(function(authUser) { //if Authentication changes such as login or logout
+        if (authUser) {
+            var userRef = ref.child('users').child(authUser.uid);
+            var userObj = $firebaseObject(userRef);
+            $rootScope.currentUser = userObj;
+        }
+    });
+
 }]);
