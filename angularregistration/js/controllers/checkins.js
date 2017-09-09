4046f8502ca8be9ba351a6cdb54ff34d1b20c4f4
kvin1087 <kaustubh87@gmail.com>
2017-09-09 18:42:08 -0500

    Check-in Controller

---
+++ angularregistration/js/controllers/checkins.js
@@ -0,0 +1,12 @@
+myApp.controller('CheckInsController', ['$scope', 'Authentication', function($scope, Authentication) {
+
+    $scope.login = function() {
+
+        Authentication.login($scope.user);
+
+    };
+
+    $scope.logout = function() {
+        Authentication.logout();
+    };
+}]);
