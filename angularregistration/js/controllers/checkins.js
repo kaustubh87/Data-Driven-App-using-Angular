7e9b892d38513b00d745b883a5bd7c524f88d246
kvin1087 <kaustubh87@gmail.com>
2017-09-15 10:05:37 -0500

    Added ng-class to expand the form

--- angularregistration/js/controllers/checkins.js
+++ angularregistration/js/controllers/checkins.js
@@ -27,6 +27,11 @@ myApp.controller('CheckInsController', ['$scope', '$rootScope', '$location', '$r
 
         $scope.showLove = function(myCheckin) {
             myCheckin.show = !myCheckin.show;
+            if (myCheckin.userState == 'expanded') {
+                myCheckin.userState = '';
+            } else {
+                myCheckin.userState = 'expanded';
+            }
         };
 
         $scope.giveLove = function(myCheckin, myGift) {
